976c8484698427dba047e018f2ac62a2c611ad3c
owner <owner@DESKTOP-4O1GNO6>
2023-11-01 08:34:15 +0330

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+
+public class Program
+        {
+            public static void Main()
+            {
+                int EnterNumber = 5;
+                int result = EnterNumber;
+                for (int i = 1; i < EnterNumber; i++)
+                {
+                    result = result * i;
+                }
+
+                Console.WriteLine(result);
+            }
+        }//مهدی کامرانی نژاد
+    }
+    }
+}
